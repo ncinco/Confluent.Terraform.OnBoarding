3d5651d3741969de02e8cb7e3ec6f77817c4c3a0
ncinco <ncinco@gmail.com>
2023-09-08 16:11:19 +1200

    v

--- modules/topics/topics.tf
+++ modules/topics/topics.tf
@@ -2,7 +2,7 @@ resource "confluent_kafka_topic" "kafka_topics" {
   for_each           = { for topic in var.topics : topic.request_id => topic }
   
   kafka_cluster {
-    id = var.credentials.cluster_id
+    id = var.cluster_id
   }
   topic_name         = each.value.name
   partitions_count   = each.value.partitions_count
@@ -10,8 +10,8 @@ resource "confluent_kafka_topic" "kafka_topics" {
 
   # kafka cluster level api key
   credentials {
-    key              = var.credentials.key
-    secret           = var.credentials.secret
+    key              = var.cluster_key
+    secret           = var.cluster_secret
   }
   config = {
     "cleanup.policy" = each.value.cleanup_policy
